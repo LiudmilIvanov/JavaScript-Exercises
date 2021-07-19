a549a11ef6ed42d8f1fa31c774f0b7819ebdd27e
Liudmil <liusi3131@gmail.com>
2021-07-19 18:05:54 +0300

    event listener added.

--- HTTP Status Cats/template.js
+++ HTTP Status Cats/template.js
@@ -4,19 +4,34 @@ const elements = {
 };
 
 Promise.all([getTemplate('./template.hbs'),
-             getTemplate('./cat.hbs')                   
+getTemplate('./cat.hbs')
 ])
     .then(([tempSource, catSource]) => {
         Handlebars.registerPartial('cat', catSource)
         const template = Handlebars.compile(tempSource);
-        const htmlResult = template({cats});
-        
+        const htmlResult = template({ cats });
         elements.allCats().innerHTML = htmlResult;
+
+        attachEventlistener();
     })
     .catch((e) => console.error(e));
 
 
-
 function getTemplate(tempLocation) {
     return fetch(tempLocation).then((response) => response.text());
+}
+
+function attachEventlistener() {
+    elements.allCats().addEventListener('click', (e) => {
+        const { target } = e;
+        if (target.nodeName === "BUTTON") {
+            let divStatus = target.parentNode.querySelector('div.status');
+
+            if (divStatus.style.display == 'none') {
+                divStatus.style.display = 'block'
+            } else {
+            divStatus.style.display = 'none'
+            }
+        }
+    });
 }
