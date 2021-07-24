c8aa71ffa67b989c3514a10ce664cd6596bf1c69
Liudmil <liusi3131@gmail.com>
2021-07-24 15:24:09 +0300

    home and login templates visualized.

--- Team-Manager/scripts/app.js
+++ Team-Manager/scripts/app.js
@@ -1,1 +1,32 @@
- // TODO
+const router = Sammy('#main', function(){
+
+    this.use('Handlebars', 'hbs');
+
+    this.get('#/home', function() {
+        this.loadPartials({
+            'header': './templates/common/header.hbs',
+            'footer': './templates/common/footer.hbs',
+        }).then(function() {
+            this.partial('../templates/home/home.hbs');
+        });
+    });
+
+    this.get('#/login', function() {
+        this.loadPartials({
+            'header': './templates/common/header.hbs',
+            'footer': './templates/common/footer.hbs',
+            'loginForm': './templates/login/loginForm.hbs',
+        }).then(function() {
+            this.partial('../templates/login/loginPage.hbs');
+        });
+    });
+
+
+
+
+});
+
+(() => {
+    router.run('#/home')
+})();
+
