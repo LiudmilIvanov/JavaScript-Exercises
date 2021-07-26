0f72a02745184909c0f179073d01ba0439aa7713
Liudmil <liusi3131@gmail.com>
2021-07-26 15:46:42 +0300

    post - register methods created.

--- Team-Manager/scripts/app.js
+++ Team-Manager/scripts/app.js
@@ -1,49 +1,72 @@
-const router = Sammy('#main', function(){
+const UserModel = firebase.auth();
+
+const router = Sammy('#main', function () {
 
     this.use('Handlebars', 'hbs');
 
-    this.get('#/home', function() {
+    //GET
+
+    this.get('#/home', function () {
         this.loadPartials({
             'header': './templates/common/header.hbs',
             'footer': './templates/common/footer.hbs',
-        }).then(function() {
+        }).then(function () {
             this.partial('../templates/home/home.hbs');
         });
     });
 
-    this.get('#/about', function() {
+    this.get('#/about', function () {
         this.loadPartials({
             'header': './templates/common/header.hbs',
             'footer': './templates/common/footer.hbs',
-        }).then(function() {
+        }).then(function () {
             this.partial('../templates/about/about.hbs');
         });
     });
 
-    this.get('#/login', function() {
+    this.get('#/login', function () {
         this.loadPartials({
             'header': './templates/common/header.hbs',
             'footer': './templates/common/footer.hbs',
             'loginForm': './templates/login/loginForm.hbs',
-        }).then(function() {
+        }).then(function () {
             this.partial('../templates/login/loginPage.hbs');
         });
     });
 
-    this.get('#/register', function() {
+    this.get('#/register', function () {
         this.loadPartials({
             'header': './templates/common/header.hbs',
             'footer': './templates/common/footer.hbs',
             'registerForm': './templates/register/registerForm.hbs',
-        }).then(function() {
+        }).then(function () {
             this.partial('./templates/register/registerPage.hbs');
         })
     });
 
+    //POST
+    this.post('/register', function(context) {
+        const { email, password, repeatPassword} = context.params;
+
+        if (password !== repeatPassword) {
+            let err = document.querySelector('#errorBox');
+            err.textContent = 'Both passwords do not match.';
+            err.style.display = 'block';
+            return;
+        }
+
+        UserModel.createUserWithEmailAndPassword(email, password)
+            .then((createUser) => {
+
+
+
+                this.redirect('/login');
+            }).catch((e) => console.error(e));
+
+    });
 
 });
 
 (() => {
     router.run('#/home')
 })();
-
