9a9f5dc7cfeb3971ce079eb79f8ccaaaadb2f3fc
Liudmil <liusi3131@gmail.com>
2021-07-27 12:57:01 +0300

    login template added.

---
+++ Movies SPA/script.js
@@ -0,0 +1,29 @@
+const routes = {
+    'login': 'login-form-template',
+};
+
+const router = (path) => {
+    let app = document.getElementById('app');
+    let template = Handlebars.compile(document.getElementById(routes[path]).innerHTML);
+
+    app.innerHTML = template();
+    
+}
+
+function addEventListeners() {
+    document.querySelector('.navigation').addEventListener('click', navigateHandler)
+}
+
+function navigateHandler(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('nav-link')) {
+        return;
+    }
+    let url = new URL(e.target.href);
+
+    history.pushState({}, '', url.pathname);
+    router(url.pathname.slice(1));
+}
+
+addEventListeners();
