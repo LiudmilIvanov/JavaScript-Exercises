c0725bdcba13e82bd9f554a31609e64a26b13ca9
Liudmil <liusi3131@gmail.com>
2021-07-29 15:04:25 +0300

    get methods created.

--- ShoeShelf/index.js
+++ ShoeShelf/index.js
@@ -2,9 +2,36 @@ const app = Sammy('#root', function() {
 
     this.use('Handlebars', 'hbs');
 
+    //Home routes
+
     this.get('/home', function() {
-        this.loadPartial('./templates/homeGuest.hbs');
+        this.partial('./templates/homeGuest.hbs');
     });
+
+    //User routes
+    this.get('/register', function() {
+        this.partial('./templates/register.hbs');
+    });
+
+    this.get('/login', function() {
+        this.partial('./templates/login.hbs');
+    });
+
+    //Offers routes
+
+    this.get('/create-offer', function() {
+        this.partial('./templates/createOffer.hbs');
+    });
+
+    this.get('/edit-offer', function() {
+        this.partial('./templates/editOffer.hbs');
+    });
+
+    this.get('/details', function() {
+        this.partial('./templates/details.hbs');
+    });
+
+
 
 });
 
@@ -12,5 +39,5 @@ const app = Sammy('#root', function() {
 
 
 (() => {
-  app.run('/');
+  app.run('/home');
 })();
