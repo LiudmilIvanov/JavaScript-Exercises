07763d2124729ca32b471ba1b31fcabc3c3a44e0
Liudmil <liusi3131@gmail.com>
2021-07-29 16:42:41 +0300

    register functionality implemented.

--- ShoeShelf/index.js
+++ ShoeShelf/index.js
@@ -1,43 +1,78 @@
-const app = Sammy('#root', function() {
+const UserModel = firebase.auth();
+
+console.log(UserModel)
+
+const app = Sammy('#root', function () {
 
     this.use('Handlebars', 'hbs');
 
     //Home routes
 
-    this.get('/home', function() {
-        this.partial('./templates/homeGuest.hbs');
+    this.get('/home', function (context) {
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/homeGuest.hbs');
+            });
+
     });
 
     //User routes
-    this.get('/register', function() {
-        this.partial('./templates/register.hbs');
+    this.get('/register', function (context) {
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/register.hbs');
+            });
+
     });
 
-    this.get('/login', function() {
-        this.partial('./templates/login.hbs');
+    this.post('/register', function (context) {
+        const { email, password, rePassword } = context.params;
+
+        if (password !== rePassword) {
+            return;
+        }
+
+        UserModel.createUserWithEmailAndPassword(email, password)
+            .then((userData) => {
+                console.log(userData)
+
+                this.redirect('/home');
+            }).catch((e) => console.log(e));
+
+
+    });
+
+    this.get('/login', function (context) {
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/login.hbs');
+            });
     });
 
     //Offers routes
 
-    this.get('/create-offer', function() {
+    this.get('/create-offer', function () {
         this.partial('./templates/createOffer.hbs');
     });
 
-    this.get('/edit-offer', function() {
+    this.get('/edit-offer', function () {
         this.partial('./templates/editOffer.hbs');
     });
 
-    this.get('/details', function() {
+    this.get('/details', function () {
         this.partial('./templates/details.hbs');
     });
 
-
-
 });
 
-
+function extendContext(context) {
+    return context.loadPartials({
+        'header': './partials/header.hbs',
+        'footer': './partials/footer.hbs',
+    })
+};
 
 
 (() => {
-  app.run('/home');
+    app.run('/home');
 })();
