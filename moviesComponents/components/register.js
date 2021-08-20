ad47924b450a34b7ff2f3733c70b85d35d025f71
Liudmil <liusi3131@gmail.com>
2021-08-20 16:25:23 +0300

    register form added.

--- moviesComponents/components/register.js
+++ moviesComponents/components/register.js
@@ -1,9 +1,59 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = (ctx) => html`
+
+<form class="text-center border border-light p-5" action="#" method="post" @submit=${ctx.onSubmit}>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" placeholder="Email" name="email" value="">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" placeholder="Password" name="password" value="">
+    </div>
+
+    <div class="form-group">
+        <label for="repeatPassword">Repeat Password</label>
+        <input type="password" class="form-control" placeholder="Repeat-Password" name="repeatPassword" value="">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Register</button>
+</form>
+
+`;
+
 export default class Register extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.innerHTML = 'I am in Register component';
+        this.render();
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        let formData = new FormData(e.target);
+
+        let email = formData.get('email');
+        let password = formData.get('password');
+        let repeatPassword = formData.get('repeatPassword');
+
+        if (password.length < 6) {
+            console.log('password too short!');
+            return;
+        }
+
+        if (password != repeatPassword) {
+            console.log('password must match!');
+            return;
+
+        }
+
+
+    }
+
+    render() {
+        render(template(this), this, { eventContext: this });
     }
 }
