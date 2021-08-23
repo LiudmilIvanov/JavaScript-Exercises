a1bbd1d78e4f785c432d5982793095a9b61c7752
Liudmil <liusi3131@gmail.com>
2021-08-23 10:58:46 +0300

    like functionality added.

--- moviesComponents/components/movie-details.js
+++ moviesComponents/components/movie-details.js
@@ -1,5 +1,12 @@
 import { html, render } from 'https://unpkg.com/lit-html?module';
+import { getUserData } from '../services/authServices.js';
 import { getOneMovie } from '../services/movieService.js'
+import { likeMovie } from '../services/movieService.js';
+
+const hasLiked = (likes, email) => {
+    return Object.values(likes)
+        .some(like => like.creator == email);
+} 
 
 const template = (ctx) => html`
     <div class="container">
@@ -12,22 +19,40 @@ const template = (ctx) => html`
             <div class="col-md-4 text-center">
                 <h3 class="my-3 ">Movie Description</h3>
                 <p>${ctx.description}</p>
-                <a class="btn btn-danger" href="#">Delete</a>
-                <a class="btn btn-warning" href="#">Edit</a>
-                <a class="btn btn-primary" href="#">Like</a>
-                <span class="enrolled-span">Liked 1</span>
+                ${ctx.creator == ctx.user.email
+                ? html` <a class="btn btn-danger" href="#">Delete</a>
+                <a class="btn btn-warning" href="#">Edit</a>`
+                : 
+                html`
+                ${hasLiked(ctx.likes, ctx.user.email)
+                    ? html`<a class="btn btn-primary" href="@click=${ctx.onLike}">Like</a>`
+                    : html` <span class="enrolled-span">Liked ${Object.keys(ctx.likes).length}</span>`
+                }`
+                }
             </div>
         </div>
     </div>
 `;
 
 export default class MovieDetails extends HTMLElement {
+    constructor() {
+        super();
+
+        this.user = getUserData();
+    }
+
     connectedCallback() {
         getOneMovie(this.location.params.id)
             .then(data => {
                 Object.assign(this, data);
                 this.render();
-                console.log('it worked!')
+            })
+    }
+
+    onLike(e) {
+        likeMovie(this.user.email)
+            .then(data => {
+                this.render();
             })
     }
 
